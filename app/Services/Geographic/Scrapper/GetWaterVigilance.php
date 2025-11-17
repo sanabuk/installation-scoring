ac5a1e6d69a199fdc49d1fe0c748625206af59e6
sanabuk <guillaume.ferard@orange.fr>
2025-11-17 22:24:25 +0100

    Features IncomingTax and WaterVigilance

---
+++ app/Services/Geographic/Scrapper/GetWaterVigilance.php
@@ -0,0 +1,32 @@
+<?php 
+
+namespace App\Services\Geographic\Scrapper;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\JsonResponse;
+
+class GetWaterVigilance
+{
+    public float $lon;
+    public float $lat;
+
+    public function __construct(float $lon, float $lat)
+    {
+        $this->lon = $lon;
+        $this->lat = $lat;
+    }
+
+    public function __invoke():JsonResponse
+    {
+        $response = Http::get('https://api.vigieau.beta.gouv.fr/api/zones',[
+            'lon' => $this->lon,
+            'lat' => $this->lat
+        ]);
+
+        if ($response->successful()) {
+            return $response->json();
+        } else {
+            return response()->json(['error' => 'Unable to fetch data'], $response->status());
+        }
+    }
+}
