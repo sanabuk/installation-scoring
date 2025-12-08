065b51bee784137738d2f241503006d334e63109
sanabuk <guillaume.ferard@orange.fr>
2025-12-09 00:21:07 +0100

    WIP scoring template. Warning: the controller's business logic must be moved elsewhere.

--- app/Http/Controllers/ScoringController.php
+++ app/Http/Controllers/ScoringController.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Storage;
 
 class ScoringController extends Controller
 {
+    const INCOMING_TAX_SCORING_THRESHOLDS = [
+        'TAXABLE_HOUSEHOLDS_PERCENT' => 45.3,
+        'AVERAGE_SALARY_TAX' => 34.35,
+        'AVERAGE_PENSION_TAX' => 26.14
+    ];
+
     public function startScoring(Request $request)
     {
         Log::info('Start scoring request received', ['data' => $request->all()]);
@@ -18,6 +24,28 @@ class ScoringController extends Controller
 
     public function getScoringResult($code_insee)
     {
-        return view('score', ['datas' => json_decode(Storage::get('scoring_results_41139_test@test.fr.json')),'code_insee' => $code_insee]);
+        $datas = json_decode(Storage::get('scoring_results_37196_guillaume@test.fr.json'));
+        foreach ($datas as $key => $data) {
+            $scoringIncomingData = $this->scoringFromIncomingTax($data->incoming_tax[0][0]);
+            $datas[$key]->scoring_incoming_tax = $scoringIncomingData;
+        }
+        return view('score', ['datas' => $datas,'code_insee' => $code_insee]);
+    }
+
+    private function scoringFromIncomingTax($incomingTaxDTO)
+    {
+        $percent_taxable_households = $incomingTaxDTO->number_of_taxed_households/$incomingTaxDTO->number_of_taxable_households * 100;
+        $scoring_percent_taxable_households = ($percent_taxable_households / self::INCOMING_TAX_SCORING_THRESHOLDS['TAXABLE_HOUSEHOLDS_PERCENT']) * 100;
+        $average_salary_tax = $incomingTaxDTO->amount_by_salary/$incomingTaxDTO->number_of_households_taxed_on_salary;
+        $scoring_average_salary_tax = ($average_salary_tax / self::INCOMING_TAX_SCORING_THRESHOLDS['AVERAGE_SALARY_TAX']) * 100;
+        $average_pension_tax = $incomingTaxDTO->amount_by_pension/$incomingTaxDTO->number_of_households_taxed_on_pension;
+        $scoring_average_pension_tax = ($average_pension_tax / self::INCOMING_TAX_SCORING_THRESHOLDS['AVERAGE_PENSION_TAX']) * 100;
+        $scoring_incoming_tax = round(($scoring_percent_taxable_households + $scoring_average_salary_tax + $scoring_average_pension_tax) / 3,2);
+        return [
+            'scoring_percent_taxable_households' => round($scoring_percent_taxable_households,2),
+            'scoring_average_salary_tax' => round($scoring_average_salary_tax,2),
+            'scoring_average_pension_tax' => round($scoring_average_pension_tax,2),
+            'scoring_incoming_tax' => $scoring_incoming_tax
+        ];
     }
 }
--- resources/views/score.blade.php
+++ resources/views/score.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Scoring</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     @vite(['resources/css/style.css', 'resources/js/app.js'])
 </head>
 <body>
@@ -34,9 +37,13 @@
             <div class="card-city" style="@if($city->code_insee == $code_insee) border:3px solid var(--accent) @endif">
                 <h3 style="color:var(--soil)">{{ $city->name }}</h3>
                 <ul>
+                    <li>Durée : {{ $city->limit_duration/60 }} min</li>
                     <li>Population : {{ $city->population }}</li>
                     <li>Nb de foyers : {{ $city->incoming_tax[0][0]->number_of_taxable_households }}</li>
                     <li>Code insee : {{ $city->code_insee }}</li>
+                    <li>Score foyers imposables : {{ $city->scoring_incoming_tax['scoring_percent_taxable_households'] }}</li>
+                    <li>Score salaires : {{ $city->scoring_incoming_tax['scoring_average_salary_tax'] }}</li>
+                    <li>Score retraites/pensions : {{ $city->scoring_incoming_tax['scoring_average_pension_tax'] }}</li>
                 </ul>
             </div>
         @endforeach
